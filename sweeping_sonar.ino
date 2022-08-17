81d88ea25f279cab302db3b0364994679adc2e9a
geofrancis <o.o.o.o@hotmail.co.uk>
2022-08-17 11:04:06 +0100

    Update sweeping_sonar.ino

--- sweeping_sonar.ino
+++ sweeping_sonar.ino
@@ -11,16 +11,16 @@
 int pothigh = 700;
 int potlow = 300;
 
+int res = 2;
+int FOV = 120;
 
 
 int potiValue = 0;
-int FOV = 120; //multiple of res and even(res is 3 degree for the TF02-pro)
+ //multiple of res and even(res is 3 degree for the TF02-pro)
 int lidarAngle = 0;
 int messageAngle = 0;
-int res = 2;
 uint16_t distances[72];
 int target = 0;
-
 unsigned char data_buffer[4] = {0};
 int adjusted = 0;
 int distance = 0;
@@ -32,18 +32,59 @@ byte received;
 HardwareSerial Serial1(USART1);
 HardwareSerial Serial2(USART2);
 
-Servo myservo;  // create servo object to control a servo
-int posi = 0;    // variable to store the servo position
+
+class Sweeper
+{
+ Servo servo;              // the servo
+ int pos;              // current servo position 
+ int increment;        // increment to move for each interval
+ int  updateInterval;      // interval between updates
+ unsigned long lastUpdate; // last update of position
+
+public: 
+ Sweeper(int interval)
+ {
+   updateInterval = interval;
+   increment = 1;
+ }
+ 
+ void Attach(int pin)
+ {
+   servo.attach(pin);
+ }
+ 
+ void Detach()
+ {
+   servo.detach();
+ }
+ 
+ void Update()
+ {
+   if((millis() - lastUpdate) > updateInterval)  // time to update
+   {
+     lastUpdate = millis();
+     pos += increment;
+     servo.write(pos);
+     Serial.println(pos);
+     if ((pos >= 180) || (pos <= 0)) // end of sweep
+     {
+       // reverse direction
+       increment = -increment;
+     }
+   }
+ }
+};
+
 
 char serial_buffer[15];
-
+Sweeper sweeper1(20);
 
 void setup() {/////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Serial1.begin(FCbaud); // USB
   Serial2.begin(sonarbaud);
+  sweeper1.Attach(A1);
   
-  myservo.attach(A1);  // attaches the servo on pin 9 to the servo object
-  myservo.write(180); 
+  flushSerial2();
 
   memset(distances, UINT16_MAX, sizeof(distances)); // Filling the distances array with UINT16_MAX
  }
@@ -51,32 +92,23 @@ void setup() {//////////////////////////////////////////////////////////////////
 
 
 // Scanning fuction. Adapt to your needs
-int16_t tfDist = 0;    // Distance to object in centimeters
+int16_t Dist = 0;    // Distance to object in centimeters
 
 
 void loop() {///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 potiValue = analogRead(POTI_PIN);
-//Serial.print ("potValue "); 
-//Serial.println (potiValue);
-lidarAngle = map(potiValue, potlow, pothigh, -60, 60);          // Adjust for the poti you use
-//Serial.print ("message angle ");
-//Serial.println (messageAngle);
-//Serial.print ("lidar angle ");
-//Serial.println (lidarAngle);
+lidarAngle = map(potiValue, potlow, pothigh, -60, 60);          
 messageAngle = map(lidarAngle, -FOV/2, FOV/2, 0, FOV);
 
 if(lidarAngle%res <=target)
   { // get a distance reading for each res (3 degree) step
 
- 
-//Serial.print ("posi ");
-//Serial.println (posi);
 
-readsonar();
+
 moveservo();
 send_pos();
-
+readsonar();
   
   }
 
@@ -84,22 +116,9 @@ send_pos();
 
 void moveservo(){
 
- if (lidarAngle <= -FOV/2)  
-  {
-    
- for (posi = 0; posi <= 180; posi += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(posi);              // tell servo to go to position in variable 'pos'
-
-  }
-  }
-    if (lidarAngle >= FOV/2)
+sweeper1.Update();
+}
  
-  for (posi = 180; posi >= 0; posi -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(posi);              // tell servo to go to position in variable 'pos'
-
-  }
- }
 
 
   
@@ -108,7 +127,7 @@ void readsonar(){
  
    if (Serial2.available() > 0) {
     Serial2.write(0x55);
-       delay(1);
+       delay(15);
     // Check for packet header character 0xff
     if (Serial2.read() == 0xff) {
       // Insert header into array
@@ -122,10 +141,10 @@ void readsonar(){
       CS = data_buffer[0] + data_buffer[1] + data_buffer[2];
       // If checksum is valid compose distance from data
       if (data_buffer[3] == CS) {
-        range = 0.1 * (data_buffer[1] << 8) + data_buffer[2];
+        range = 1 * (data_buffer[1] << 8) + data_buffer[2];
          //adjusting for the speed of sound in water
        
-      tfDist = range;
+      Dist = range/10;
       }
     }
   }
@@ -136,27 +155,46 @@ void send_pos(){////////////////////////////////////////////////////////////////
 //MAVLINK DISTANCE MESSAGE
   int sysid = 1;                   
   //< The component sending the message.
-  int compid = 158;    
+  int compid = 196;    
   uint64_t time_usec = 0; /*< Time since system boot*/
   uint8_t sensor_type = 0;
-  distances[messageAngle/res] = tfDist-2.0f; //UINT16_MAX gets updated with actual distance values
+  distances[messageAngle/res] = Dist-2.0f; //UINT16_MAX gets updated with actual distance values
   uint8_t increment = 3;
   uint16_t min_distance = 30; /*< Minimum distance the sensor can measure in centimeters*/
   uint16_t max_distance = 500; /*< Maximum distance the sensor can measure in centimeters*/
   float increment_f = 0;
   float angle_offset = -FOV/2;
   uint8_t frame = 12;
+  uint8_t system_type = MAV_TYPE_GENERIC;
+  uint8_t autopilot_type = MAV_AUTOPILOT_INVALID;
+  uint8_t system_mode = MAV_MODE_PREFLIGHT; ///< Booting up
+  uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
+  uint8_t system_state = MAV_STATE_STANDBY; ///< System ready for flight
+
   // Initialize the required buffers
   mavlink_message_t msg;
   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
-
+ int type = MAV_TYPE_GROUND_ROVER;
   // Pack the message
+  
  mavlink_msg_obstacle_distance_pack(sysid,compid,&msg,time_usec,sensor_type,distances,increment,min_distance,max_distance,increment_f,angle_offset,frame);
-
-  // Copy the message to the send buffer
   uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-  // Send the message (.write sends as bytes) 
-  //delay(1);
   Serial1.write(buf, len);
+  
+  // Pack the message
+  //mavlink_msg_heartbeat_pack(sysid,compid, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
+  mavlink_msg_heartbeat_pack(1,196, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
+ 
+  // Copy the message to the send buffer  
+  len = mavlink_msg_to_send_buffer(buf, &msg);
+  Serial1.write(buf, len);
+  
 
 }
+
+
+// Flushes the INPUT serial buffer
+void flushSerial2(){
+  while(Serial2.available()){Serial2.read();}
+}
+
